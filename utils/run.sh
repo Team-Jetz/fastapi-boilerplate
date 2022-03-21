d41d2ea110d225b35200a6e83b42a1136a91fd99
Jethro Sumague <jethro.sumague00@gmail.com>
2022-03-22 03:38:05 +0800

    Added Pytest for testing

--- utils/run.sh
+++ utils/run.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-
-# python manage.py makemigrations
-
-# python manage.py migrate
-
-#python manage.py collectstatic --no-input
-#python manage.py collectstatic --no-post-process --no-input
-
-# gunicorn core.wsgi --bind=0.0.0.0:80 --timeout 120
 uvicorn app.main --bind=0.0.0.0:80 --timeout 120
-
-# --timeout 1800 --graceful-timeout 1800
-# --workers=2
